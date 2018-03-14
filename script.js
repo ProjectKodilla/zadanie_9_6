fa0a406ec781da15e2c4bcfefde002cc50eb8272
Marcin <36379394+MarcinBB@users.noreply.github.com>
2018-03-14 20:09:34 +0100

    Update script.js
    add_changes

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var list = document.getElementById("list");
-console.log(document.getElementById('list'));
 var add = document.getElementById("addElem");
-console.log(document.getElementById("addElem"));
 
 add.addEventListener("click", function() {
     var documents = document.querySelectorAll("#list > li"); 
-    console.log(documents);
     var element = document.createElement("li");
-    element.innerHTML = "item" + " " + documents.length;
+    element.innerHTML = "item " + documents.length;
     list.appendChild(element);
 }
 ); 
