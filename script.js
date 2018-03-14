5a1af1d73e5c24e5684f4331aed474f55e0bcd06
Marcin Bogdanski <marcin@bogdanski.info>
2018-03-14 10:30:21 +0100

    finished

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var list = document.getElementById("list");
+console.log(document.getElementById('list'));
 var add = document.getElementById("addElem");
+console.log(document.getElementById("addElem"));
 
-add.addEventListener ("click", function() {
+add.addEventListener("click", function() {
+    var documents = document.querySelectorAll("#list > li"); 
+    console.log(documents);
     var element = document.createElement("li");
-    element.innerHTML = "item";
-    list.appendChild(element + "1");
-
-});
+    element.innerHTML = "item" + " " + documents.length;
+    list.appendChild(element);
+}
+); 
